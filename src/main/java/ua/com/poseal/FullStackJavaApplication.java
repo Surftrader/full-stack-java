2e0e6faf26e0fec2f7593cb359c998168224cd32
Sergiy Poddubka <poseal@gmail.com>
2021-03-17 22:40:44 +0200

    init full-stack-project

---
+++ src/main/java/ua/com/poseal/FullStackJavaApplication.java
@@ -0,0 +1,13 @@
+package ua.com.poseal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/com/poseal/FullStackJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.poseal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
